7479591882790c8e311409b8684215e36a1e4735
root <root@www.drbrynar.se>
2016-01-09 18:40:56 -0500

    changing registration to sending email with user/password and validating pictures

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug import secure_filename
 from flask_recaptcha import ReCaptcha
 from flask.ext.images import Images
-import hashlib, datetime, random, os
+from email.mime.text import MIMEText
+import hashlib, datetime, random, os, smtplib, string, imghdr, shutil
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
@@ -19,15 +20,44 @@ login_manager.login_view = 'login'
 
 images = Images(app)
 
+#temp folder to save files befor moved to upload folder
+app.config['TMP_FOLDER'] = '/opt/www.drbrynar.se/tmp/'
+
 #folder to save uploaded files to
 app.config['UPLOAD_FOLDER'] = '/opt/www.drbrynar.se/static/pictures/'
+
 #allowed file exstenstion when uploading a file
-app.config['ALLOWED_EXTENSIONS'] = set(['tiff','jpeg','jpg','png','img','tif','gif','bmp'])
+app.config['ALLOWED_EXTENSIONS'] = set(['tiff','jpeg','jpg','png','gif','bmp'])
+
 #max size of the uploaded files
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 
 def allowed_file(filename):
   return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+def sendPasswordByEmail(toEmail,username,password):
+  mailSubject = "Login details on www.drbrynar.se"
+  mailText = "Username: " + username + "\n" + "Password: " + password + "\n\n" + "Kind regards" + "\n" + "DrBrynar" 
+  msg = MIMEText(mailText)
+
+  msg["Subject"] = mailSubject
+  msg["From"] = "no-reply@www.drbrynar.se"
+  msg["To"] = toEmail
+
+  s = smtplib.SMTP('localhost')
+  s.sendmail(msg["From"], msg["To"], msg.as_string())
+  s.quit()
+
+def genID(size=32):
+  chars=string.ascii_uppercase + string.digits
+  return ''.join(random.choice(chars) for _ in range(size))
+
+
+def generatePassword(length):
+  chars = string.ascii_letters + string.digits + '!@#$%&*()/[]=?'
+  random.seed = (os.urandom(1024))
+
+  return ''.join(random.choice(chars) for i in range(length))
 
 @app.before_first_request
 def setup():
@@ -47,19 +77,29 @@ def load_user(id):
 @app.route("/")
 def start():
   if request.method == 'GET':
-    db.session.add(Logg("GET start", request.remote_addr,request.user_agent.string))
+    #logg to db
+    db.session.add(Logg("GET /", request.remote_addr,request.user_agent.string))
     db.session.commit()
-    rand = random.randrange(0, db.session.query(Picture).count()) 
-    pic_row = db.session.query(Picture)[rand]
-    #pic_filename = url_for('static', filename ="pictures/" + pic_row.filename)
-    return render_template('start.html', pic = pic_row)
+
+    #if there has not been any picture upload show error
+    if db.session.query(Picture).count() == 0:
+      return render_template('error.html', error_message = "No pictures is uploaded :(")
+    #show random picture to user
+    else:
+      rand = random.randrange(0, db.session.query(Picture).count()) 
+      pic_row = db.session.query(Picture)[rand]
+
+      return render_template('start.html', pic = pic_row)
 
 @app.route("/show_all_pics")
 def show_all_pics():
   if request.method == 'GET':
-    db.session.add(Logg("GET show_all", request.remote_addr,request.user_agent.string))
+    #logg to db
+    db.session.add(Logg("GET /show_all_pics", request.remote_addr,request.user_agent.string))
     db.session.commit()
+
     pics = db.session.query(Picture).all()
+
     return render_template('show_all_pics.html',pics = pics)
 
 
@@ -68,89 +108,200 @@ def show_all_pics():
 @login_required
 def upload():
   if request.method == 'GET':
-    db.session.add(Logg("GET upload", request.remote_addr,request.user_agent.string))
+    #logg to db
+    db.session.add(Logg("GET /upload", request.remote_addr,request.user_agent.string))
     db.session.commit()
+
     return render_template('upload.html')
+
   if request.method == 'POST':
-    db.session.add(Logg("POST upload", request.remote_addr,request.user_agent.string))
+    #logg to db
+    db.session.add(Logg("POST /upload", request.remote_addr,request.user_agent.string))
     db.session.commit()
+
+    #get file
     file = request.files['file']
-    if file and allowed_file(file.filename):
+
+    if file:
+      #if file exstienstion is not allowed logg and show error
+      if not allowed_file(file.filename):
+        #logg to db
+        db.session.add(Logg("POST /upload: files exstenstion is not allowed", request.remote_addr,request.user_agent.string))
+        db.session.commit()
+
+        return render_template('error.html', error_message = "File is not of allowed type. Allowed files are: " + str(app.config['ALLOWED_EXTENSIONS']))
+
+      #create secure filename
       filename = secure_filename(file.filename)
-      if os.path.exists(app.config['UPLOAD_FOLDER'] + "/" + filename):
-        return render_template('error.html', error_message = "You need to choose another filename :(")
-      db.session.add(Picture(2, filename, request.remote_addr,request.user_agent.string))
+
+      #save file in tmp folder
+      tmpID = genID()
+      tmpFolder = app.config['TMP_FOLDER'] + tmpID + "/" 
+      os.mkdir(tmpFolder)
+      file.save(os.path.join(tmpFolder, filename))
+
+      #get checksums of file
+      file_in_mem = open(tmpFolder + filename,"rb").read()
+      md5 = hashlib.md5(file_in_mem).hexdigest()
+      sha1 = hashlib.sha1(file_in_mem).hexdigest()
+      sha256 = hashlib.sha256(file_in_mem).hexdigest()
+
+      #check that file is a picture, if file is not a picture do not move file to upload folder
+      fileType = imghdr.what(tmpFolder + filename)
+      if not fileType in app.config['ALLOWED_EXTENSIONS']:
+        #logg to db
+        db.session.add(Logg("POST /upload: uploaded file can not be validated with imghdr", request.remote_addr,request.user_agent.string))
+        db.session.commit()
+
+        return render_template('error.html', error_message = "File is not of allowed type. Allowed files are: " + str(app.config['ALLOWED_EXTENSIONS']))
+
+      if request.form["description"]:
+        description = request.form["description"]
+      else:
+        description = "none"
+
+      #add file data to db
+      picture = Picture(current_user.get_id(), filename, description, md5, sha1, sha256, request.remote_addr, request.user_agent.string)
+      db.session.add(picture)
       db.session.commit()
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      db.session.add(Logg("POST upload file: " + filename, request.remote_addr,request.user_agent.string))
+
+      #get id from db
+      newFilename = str(picture.id)
+
+      #move file to upload folder
+      shutil.move(tmpFolder + filename, app.config['UPLOAD_FOLDER'] + newFilename)
+
+      #remove tmp folder
+      shutil.rmtree(tmpFolder)
+
+      #logg to db
+      db.session.add(Logg("POST /upload: filename " + filename + " has been uploaded", request.remote_addr,request.user_agent.string))
       db.session.commit()
-      return render_template('start.html')
-    return redirect(url_for('upload'))
+
+      #return render_template('start.html')
+      return redirect(url_for('start'))
+
+    #if any of the form is empty logg and show error
+    else:
+      #logg to db
+      db.session.add(Logg("POST /upload: empty form", request.remote_addr,request.user_agent.string))
+      db.session.commit()
+
+      return render_template('error.html', error_message = "Empty form")
 
 @app.route("/login", methods=['POST', 'GET'])
 @app.route("/l", methods=['POST', 'GET'])
 def login():
   if request.method == 'GET':
-    db.session.add(Logg("GET login", request.remote_addr,request.user_agent.string))
+    #logg to db
+    db.session.add(Logg("GET /login", request.remote_addr,request.user_agent.string))
     db.session.commit()
+
     return render_template('login.html')
+
   if request.method == 'POST':
-    db.session.add(Logg("POST login", request.remote_addr,request.user_agent.string))
+    #logg to db
+    db.session.add(Logg("POST /login", request.remote_addr,request.user_agent.string))
     db.session.commit()
-    if request.form["email"] and request.form["password"]:
+
+    if request.form["username"] and request.form["password"]:
       hashed_password = hashlib.sha512(request.form["password"] + app.config["USER_PW_SALT"]).hexdigest()
-      users = db.session.query(User).filter(User.email == request.form["email"], User.password == hashed_password).first()
-      if users is None:
-        db.session.add(Logg("POST login failed login with email: " + request.form["email"] + " password: " + request.form["password"], request.remote_addr,request.user_agent.string))
+      user = db.session.query(User).filter(User.username == request.form["username"], User.password == hashed_password).first()
+
+      #if login files logg and show error
+      if user is None:
+        #logg to db
+        db.session.add(Logg("POST /login: failed login with username: " + request.form["username"] + " password: " + request.form["password"], request.remote_addr,request.user_agent.string))
         db.session.commit()
+
         return render_template('error.html', error_message = "Username or/and password is invalid")
-      login_user(users)
-      db.session.add(Logg("POST login succesfully login with email: " + request.form["email"] + " password: " + request.form["password"], request.remote_addr,request.user_agent.string))
-      db.session.commit()
-      return redirect(url_for("start"))
+
+      #if login success logg and login user
+      else:
+        #logg to db
+        db.session.add(Logg("POST /login: succesfully login with username: " + request.form["username"], request.remote_addr,request.user_agent.string))
+        db.session.commit()
+
+        login_user(user)
+
+        return redirect(url_for("start"))
+
+    #if any of the form is empty logg and show errot
     else:
-      db.session.add(Logg("POST login empty form", request.remote_addr,request.user_agent.string))
+      #logg to db
+      db.session.add(Logg("POST /login: empty form", request.remote_addr,request.user_agent.string))
       db.session.commit()
+
       return render_template('error.html', error_message = "Empty form")
 
 @app.route("/logout", methods=['GET'])
 def logout():
-  db.session.add(Logg("GET logout", request.remote_addr,request.user_agent.string))
+  #logg to db
+  db.session.add(Logg("GET /logout", request.remote_addr,request.user_agent.string))
   db.session.commit()
+
   logout_user()
+
   return redirect(url_for("start"))
   
 @app.route("/register/", methods=['POST', 'GET'])
 @app.route("/r/", methods=['POST', 'GET'])
 def register():
   if request.method == 'GET':
-    db.session.add(Logg("GET register", request.remote_addr,request.user_agent.string))
+    #logg to db
+    db.session.add(Logg("GET /register", request.remote_addr,request.user_agent.string))
     db.session.commit()
+
     return render_template('register.html')
+
   if request.method == 'POST':
-    db.session.add(Logg("POST register", request.remote_addr,request.user_agent.string))
+    #logg to db
+    db.session.add(Logg("POST /register", request.remote_addr,request.user_agent.string))
     db.session.commit()
-    if not request.form["email"] or not request.form["password1"] or not request.form["password2"]:
-      db.session.add(Logg("POST registeri empty form", request.remote_addr,request.user_agent.string))
-      db.session.commit()
-      return render_template('error.html', error_message = "Empty form")
+
+    #if recaptcha fileas logg and show error
     if not recaptcha.verify():
-      db.session.add(Logg("POST registeri recaptcha failed", request.remote_addr,request.user_agent.string))
+      #logg to db
+      db.session.add(Logg("POST /registeri: recaptcha failed", request.remote_addr,request.user_agent.string))
       db.session.commit()
+
       return render_template('error.html', error_message = "You are not a human")
-    if request.form["password1"] == request.form["password2"]:
-      hashed_password = hashlib.sha512(request.form["password1"] + app.config["USER_PW_SALT"]).hexdigest()
-      db.session.add(User(request.form["email"], hashed_password))
+
+    if request.form["email"] and request.form["username"]:
+      #check that username is not used before
+      if db.session.query(User).filter(User.username == request.form["username"]).count() != 0:
+        #logg to db
+        db.session.add(Logg("POST /register: username is used before", request.remote_addr,request.user_agent.string))
+        db.session.commit()
+
+        return render_template('error.html', error_message = "Choose another username")
+
+      #check that email is not used before
+      if db.session.query(User).filter(User.email == request.form["email"]).count() != 0:
+        #logg to db
+        db.session.add(Logg("POST /register: email is used before", request.remote_addr,request.user_agent.string))
+        db.session.commit()
+
+        return render_template('error.html', error_message = "Choose another email")
+
+      #generate password and send it to users email
+      password = generatePassword(12)
+      hashed_password = hashlib.sha512(password + app.config["USER_PW_SALT"]).hexdigest()
+      db.session.add(User(request.form["username"],request.form["email"], hashed_password))
       db.session.commit()
-      db.session.add(Logg("POST register sussesfully register email: " + request.form["email"]  + " password: " + request.form["password1"], request.remote_addr,request.user_agent.string))
-      db.session.commit()
+      sendPasswordByEmail(request.form["email"],request.form["username"],password)
+
       return redirect(url_for("login"))
+
+    #if form is empty logg and show error
     else:
-      db.session.add(Logg("POST register error password do not match password1: " + request.form["password1"]  + " password2: " + request.form["password2"], request.remote_addr,request.user_agent.string))
+      #logg to db
+      db.session.add(Logg("POST /register: empty form", request.remote_addr,request.user_agent.string))
       db.session.commit()
+
       return render_template('error.html', error_message = "Passwords do not match")
 
 if __name__ == "__main__":
-  #app.debug = True
-  app.debug = False
+  app.debug = True
+  #app.debug = False
   app.run(host='0.0.0.0',port=80)
--- models.py
+++ models.py
@@ -7,12 +7,14 @@ Base = declarative_base()
 class User(Base):
   __tablename__ = 'users'
 
-  id = Column(Integer, unique=True, primary_key=True)
+  id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
+  username = Column(String())
   email = Column(String())
   password = Column(String())
   registered_on = Column(DateTime())
 
-  def __init__(self, email, password):
+  def __init__(self, username, email, password):
+    self.username = username
     self.email = email
     self.password = password
     self.registered_on = time.strftime('%Y-%m-%d %H:%M:%S')
@@ -35,24 +37,35 @@ class User(Base):
 class Picture(Base):
   __tablename__ = 'pictures'
 
-  id = Column(Integer, unique=True, primary_key=True)
+  id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
   user_id = Column(Integer())
   filename = Column(String())
+  description = Column(String())
+  md5 = Column(String())
+  sha1 = Column(String())
+  sha256 = Column(String())
   uploader_ip = Column(String())
   uploader_useragent = Column(String())
-  registered_on = Column(DateTime())
+  uploaded_on = Column(DateTime())
 
-  def __init__(self, user_id, filename,uploader_ip, uploader_useragent):
+  def __init__(self, user_id, filename,description,md5,sha1,sha256,uploader_ip, uploader_useragent):
     self.user_id = user_id
     self.filename = filename
+    self.description = description
+    self.md5 = md5
+    self.sha2 = sha1
+    self.sha256 = sha256
     self.uploader_ip = uploader_ip
     self.uploader_useragent = uploader_useragent
     self.uploaded_on = time.strftime('%Y-%m-%d %H:%M:%S')
 
+  def id_as_str(self):
+    return str(self.id)
+
 class Logg(Base):
   __tablename__ = 'loggs'
 
-  id = Column(Integer, unique=True, primary_key=True)
+  id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
   action = Column(String())
   ip = Column(String())
   useragent = Column(String())
